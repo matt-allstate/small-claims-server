9347e6afe2225dd4277179f9a6961333b8f90e23
Matt <matt@multicode.co.uk>
2023-01-12 13:04:00 +0000

    first commit

---
+++ src/main/java/com/allstate/smallclaimsserver/SmallClaimsServerApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.smallclaimsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallClaimsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallClaimsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allstate/smallclaimsserver/SmallClaimsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.smallclaimsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallClaimsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
